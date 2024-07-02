0669a4f36f474cddcc512076e8b9d67868d95c37
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2024-07-02 13:28:11 -0300

    Interface Speakable and Classes Cat and Dog done

---
+++ StaticAndInterface/src/Cat.java
@@ -0,0 +1,5 @@
+public class Cat implements  Speakable{
+    public void speak() {
+        System.out.println("miau miau miau miau miau miau miau");
+    }
+}
---
+++ StaticAndInterface/src/Dog.java
@@ -0,0 +1,5 @@
+public class Dog implements Speakable{
+    public void speak() {
+        System.out.println("huff huff huff huff huff huff!");
+    }
+}
